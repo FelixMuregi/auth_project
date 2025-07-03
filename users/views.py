eb43db242b46e57f2ebb6e4afe7c23cc866e5c8b
Felix Muregi <felixmuregi@gmail.com>
2025-07-03 05:53:50 +0300

    Initial commit: Django auth project

---
+++ users/views.py
@@ -0,0 +1,35 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=201)
+        return Response(serializer.errors, status=400)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(username=user.username, password=password)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token)
+            })
+        return Response({"error": "Invalid credentials"}, status=401)
+
